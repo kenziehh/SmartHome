29233f6e09eb4db4e15a047522d19529b6c026a7
kenziehh <ghaisani.nurani@gmail.com>
2023-10-15 18:56:42 +0700

    fix class name error

--- Src/SmartHome.py
+++ Src/SmartHome.py
@@ -15,9 +15,9 @@ class Room:
     def calculate_total_energy_consumption(self, current_condition):
         total_consumption = 0
         for device, quantity in self.devices.items():
-            if isinstance(device, AutomaticControlledDevice) and device.is_active(current_condition):
+            if isinstance(device, AutomaticDevice) and device.is_active(current_condition):
                 total_consumption += device.get_total_energy_consumption(quantity)
-            elif not isinstance(device, AutomaticControlledDevice):
+            elif not isinstance(device, AutomaticDevice):
                 total_consumption += device.get_total_energy_consumption(quantity)
         self.total_energy_consumption = total_consumption
         
